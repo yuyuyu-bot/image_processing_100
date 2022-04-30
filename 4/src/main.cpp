60e32769342e6cea5f3a12208c6e91475d59c3ca
yuyuyu <yuyuyu.nttmmoo@gmail.com>
2022-04-30 20:32:38 +0900

    cpp otsu thresh

---
+++ 4/src/main.cpp
@@ -0,0 +1,73 @@
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+
+#include "common.hpp"
+#include "threshold_otsu_cpp.hpp"
+// #include "threshold_otsu_cuda.hpp"
+// #include "threshold_otsu_neon.hpp"
+
+
+int main(int argc, char** argv) {
+    if (argc != 4) {
+        std::cout << "usage: " << argv[0] << " image_path width height threshold" << std::endl;
+        return 0;
+    }
+
+    constexpr auto iteration = 1;
+
+    using IMG_T = std::uint8_t;
+
+    const auto width = std::stoul(argv[2]);
+    const auto height = std::stoul(argv[3]);
+    const Image<IMG_T, 1> src_img(argv[1], width, height);
+    const auto src = src_img.data();
+
+    Image<IMG_T, 1> dst_cpp(width, height);
+    Image<IMG_T, 1> dst_neon(width, height);
+    Image<IMG_T, 1> dst_cuda(width, height);
+
+    {
+        const auto dst = dst_cpp.data();
+        const auto duration =
+            measure(iteration, cpp::threshold_otsu, src, dst, width, height);
+        std::cout << "durations:" << std::endl;
+        std::cout << "\tcpp : " << duration << " [usec]" << std::endl;
+    }
+
+    // {
+    //     const auto dst = dst_neon.data();
+    //     const auto duration =
+    //         measure(iteration, neon::threshold_otsu, src, dst, width, height, thresh);
+    //     std::cout << "\tneon: " << duration << " [usec]" << std::endl;
+
+    //     compare_images(dst_cpp, dst_neon);
+    // }
+
+    // {
+    //     IMG_T* d_src;
+    //     IMG_T* d_dst;
+    //     cudaMalloc((void**)&d_src, width * height * sizeof(IMG_T));
+    //     cudaMalloc((void**)&d_dst, width * height * sizeof(IMG_T));
+    //     cudaMemcpy(d_src, src, width * height * sizeof(IMG_T), cudaMemcpyHostToDevice);
+
+    //     const auto duration =
+    //         measure(iteration, cuda::threshold_otsu, d_src, d_dst, width, height, thresh);
+    //     std::cout << "\tcuda: " << duration << " [usec]" << std::endl;
+
+    //     cudaMemcpy(dst_cuda.data(), d_dst, width * height * sizeof(IMG_T), cudaMemcpyDeviceToHost);
+    //     cudaFree((void*)d_src);
+    //     cudaFree((void*)d_dst);
+
+    //     compare_images(dst_cpp, dst_cuda);
+    // }
+
+    dst_cpp.write("cpp.png");
+    dst_neon.write("neon.png");
+    dst_cuda.write("cuda.png");
+
+    return 0;
+}
---
+++ 4/src/threshold_otsu_cpp.hpp
@@ -0,0 +1,63 @@
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <numeric>
+
+
+namespace cpp {
+
+void threshold_otsu(const std::uint8_t* const src, std::uint8_t* const dst,
+                    const std::size_t width, const std::size_t height) {
+    constexpr auto min_value = std::numeric_limits<std::uint8_t>::min();
+    constexpr auto max_value = std::numeric_limits<std::uint8_t>::max();
+
+    auto histogram = std::array<int, max_value - min_value + 1>();
+    auto value_sum = 0ull;
+    auto square_value_sum = 0ull;
+    for (std::size_t i = 0; i < width * height; i++) {
+        histogram[src[i]]++;
+        value_sum += src[i];
+        square_value_sum += src[i] * src[i];
+    }
+    const auto mean_all = static_cast<float>(value_sum) / (width * height);
+
+    auto maxS = 0.f;
+    auto thresh = 0;
+    auto n1 = 0, n2 = static_cast<int>(width * height);
+    auto sum1 = 0ull, sum2 = value_sum;
+    auto square_sum1 = 0ull, square_sum2 = square_value_sum;
+    for (std::size_t i = 1; i < histogram.size(); i++) {
+        n1 += histogram[i - 1];
+        n2 -= histogram[i - 1];
+
+        if (n1 == 0) {
+            continue;
+        }
+        if (n2 <= 0) {
+            break;
+        }
+
+        sum1 += (i - 1) * histogram[i - 1];
+        sum2 -= (i - 1) * histogram[i - 1];
+        square_sum1 += (i - 1) * (i - 1) * histogram[i - 1];
+        square_sum2 -= (i - 1) * (i - 1) * histogram[i - 1];
+
+        const auto mean1 = static_cast<float>(sum1) / n1;
+        const auto mean2 = static_cast<float>(sum2) / n2;
+        const auto dev1 = static_cast<float>(square_sum1) / n1 - mean1 * mean1;
+        const auto dev2 = static_cast<float>(square_sum2) / n2 - mean2 * mean2;
+        const auto S = (n1 * (mean1 - mean_all) * (mean1 - mean_all) +
+                        n2 * (mean2 - mean_all) * (mean2 - mean_all)) / (n1 * dev1 + n2 * dev2);
+
+        if (maxS < S) {
+            maxS = S;
+            thresh = i;
+        }
+    }
+
+    for (std::size_t i = 0; i < width * height; i++) {
+        dst[i] = src[i] < thresh ? min_value : max_value;
+    }
+}
+
+}  // namespace cpp
