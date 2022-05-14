034e8d3e401c22be6b8275bc6a75388b2177ccd6
yuyuyu <yuyuyu.nttmmoo@gmail.com>
2022-05-14 22:48:56 +0900

    use warp shuffle

--- 11/src/mean_filter_kernel.cu
+++ 11/src/mean_filter_kernel.cu
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <cuda_runtime.h>
 
 #include "cuda_safe_call.hpp"
@@ -7,6 +8,7 @@
 namespace {
 
 using AccumulatorType = std::uint32_t;
+constexpr auto SUM_BUFFER_SIZE = 8;
 
 __device__ int clamp(int a, int b, int c) {
     return max(b, min(a, c));
@@ -17,15 +19,17 @@ __global__ void mean_filter_kernel(const std::uint8_t* const src, std::uint8_t* 
                                    const std::size_t ksize, const int pixel_per_thread) {
     const int pidx = blockIdx.x * pixel_per_thread;
     const auto kidx = threadIdx.x;
+    const auto lane_idx = kidx % 32;
+    const auto warp_idx = kidx / 32;
+    const auto num_warps = (blockDim.x + 31) / 32;
 
     const auto stride = width * 3;
     const auto khalf = static_cast<int>(ksize) >> 1;
     const auto ksquare = ksize * ksize;
 
-    extern __shared__ AccumulatorType s_kernel_buffer[];
-    AccumulatorType* s_kernel_buffer_r = s_kernel_buffer;
-    AccumulatorType* s_kernel_buffer_g = s_kernel_buffer + ksize;
-    AccumulatorType* s_kernel_buffer_b = s_kernel_buffer + ksize * 2;
+    __shared__ AccumulatorType s_rsum_buffer[SUM_BUFFER_SIZE];
+    __shared__ AccumulatorType s_gsum_buffer[SUM_BUFFER_SIZE];
+    __shared__ AccumulatorType s_bsum_buffer[SUM_BUFFER_SIZE];
 
     for (int i = 0; i < pixel_per_thread; i++) {
         if (pidx + i >= width * height) {
@@ -47,28 +51,49 @@ __global__ void mean_filter_kernel(const std::uint8_t* const src, std::uint8_t* 
             local_gsum += src[sy * stride + sx * 3 + 1];
             local_bsum += src[sy * stride + sx * 3 + 2];
         }
-        s_kernel_buffer_r[kidx] = local_rsum;
-        s_kernel_buffer_g[kidx] = local_gsum;
-        s_kernel_buffer_b[kidx] = local_bsum;
-        __syncthreads();
 
-        for (std::uint32_t delta = 1; delta < ksize; delta <<= 1) {
-            if (kidx + delta < ksize) {
-                s_kernel_buffer_r[kidx] += s_kernel_buffer_r[kidx + delta];
-                s_kernel_buffer_g[kidx] += s_kernel_buffer_g[kidx + delta];
-                s_kernel_buffer_b[kidx] += s_kernel_buffer_b[kidx + delta];
+        // reduction in warp
+        const auto warp_size = min(static_cast<unsigned>(ksize), 32u);
+        for (std::uint32_t delta = 1; delta < warp_size; delta <<= 1) {
+            const auto next_rsum = __shfl_down_sync(0xffffffff, local_rsum, delta);
+            const auto next_gsum = __shfl_down_sync(0xffffffff, local_gsum, delta);
+            const auto next_bsum = __shfl_down_sync(0xffffffff, local_bsum, delta);
+            if (lane_idx + delta < warp_size) {
+                local_rsum += next_rsum;
+                local_gsum += next_gsum;
+                local_bsum += next_bsum;
             }
-            __syncthreads();
         }
 
-        const auto rsum = s_kernel_buffer_r[kidx];
-        const auto gsum = s_kernel_buffer_g[kidx];
-        const auto bsum = s_kernel_buffer_b[kidx];
-        if (kidx == 0) {
-            dst[(pidx + i) * 3 + 0] = static_cast<std::uint8_t>(rsum / (ksquare));
-            dst[(pidx + i) * 3 + 1] = static_cast<std::uint8_t>(gsum / (ksquare));
-            dst[(pidx + i) * 3 + 2] = static_cast<std::uint8_t>(bsum / (ksquare));
+        if (blockDim.x < 32) {
+            if (kidx == 0) {
+                dst[(pidx + i) * 3 + 0] = static_cast<std::uint8_t>(local_rsum / ksquare);
+                dst[(pidx + i) * 3 + 1] = static_cast<std::uint8_t>(local_gsum / ksquare);
+                dst[(pidx + i) * 3 + 2] = static_cast<std::uint8_t>(local_bsum / ksquare);
+            }
+        } else {
+            if (lane_idx == 0) {
+                s_rsum_buffer[warp_idx] = local_rsum;
+                s_gsum_buffer[warp_idx] = local_gsum;
+                s_bsum_buffer[warp_idx] = local_bsum;
+                __syncthreads();
+
+                for (std::uint32_t delta = 1; delta < num_warps; delta <<= 1) {
+                    if (warp_idx + delta < num_warps) {
+                        s_rsum_buffer[warp_idx] += s_rsum_buffer[warp_idx + delta];
+                        s_gsum_buffer[warp_idx] += s_gsum_buffer[warp_idx + delta];
+                        s_bsum_buffer[warp_idx] += s_bsum_buffer[warp_idx + delta];
+                    }
+                    __syncthreads();
+                }
+            }
+            if (kidx == 0) {
+                dst[(pidx + i) * 3 + 0] = static_cast<std::uint8_t>(s_rsum_buffer[0] / ksquare);
+                dst[(pidx + i) * 3 + 1] = static_cast<std::uint8_t>(s_gsum_buffer[0] / ksquare);
+                dst[(pidx + i) * 3 + 2] = static_cast<std::uint8_t>(s_bsum_buffer[0] / ksquare);
+            }
         }
+
         __syncthreads();
     }
 }
@@ -80,14 +105,15 @@ namespace cuda {
 
 void mean_filter(const std::uint8_t* const src, std::uint8_t* const dst,
                  const std::size_t width, const std::size_t height, const std::size_t ksize) {
+    assert(ksize < 32 * SUM_BUFFER_SIZE);
+
     constexpr auto block_dim = dim3{32};
     const auto thread_dim = dim3{static_cast<std::uint32_t>(ksize)};
     constexpr auto num_threads = block_dim.x;
     const auto pixel_per_block
         = static_cast<int>(ceilf(static_cast<float>(width * height) / num_threads));
 
-    mean_filter_kernel<<<block_dim, thread_dim, ksize * sizeof(AccumulatorType) * 3>>>(
-        src, dst, width, height, ksize, pixel_per_block);
+    mean_filter_kernel<<<block_dim, thread_dim>>>(src, dst, width, height, ksize, pixel_per_block);
     CUDASafeCall();
 }
 
