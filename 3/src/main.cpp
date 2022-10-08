e67d132bf46d4e3a45604494d9afe819a8243b72
yuyuyu <yuyuyu.nttmmoo@gmail.com>
2022-10-08 21:39:46 +0900

    update 3

--- 3/src/main.cpp
+++ 3/src/main.cpp
@@ -11,42 +11,38 @@
 
 
 int main(int argc, char** argv) {
-    if (argc != 6) {
-        std::cout << "usage: " << argv[0] << " image_path width height threshold dump_flag" << std::endl;
+    if (argc != 3) {
+        std::cout << "usage: " << argv[0] << " num_itr dump_flag" << std::endl;
         return 0;
     }
+    const auto num_itr = std::stoi(argv[1]);
+    const auto dump_flag = std::stoi(argv[2]) != 0;
 
-    constexpr auto iteration = 10;
-
-    using IMG_T = std::uint8_t;
-
-    const auto width = std::stoul(argv[2]);
-    const auto height = std::stoul(argv[3]);
-    const auto thresh = std::stoul(argv[4]);
-    const auto dump_flag = std::stoi(argv[5]) != 0;
-    const Image<IMG_T, 1> src_img(argv[1], width, height);
+    const Image<IMG_T, 1> src_img(image_gray_path, image_width, image_height);
     const auto src = src_img.data();
 
-    Image<IMG_T, 1> dst_cpp(width, height);
-    Image<IMG_T, 1> dst_neon(width, height);
-    Image<IMG_T, 1> dst_cuda(width, height);
+    Image<IMG_T, 1> dst_cpp(image_width, image_height);
+    Image<IMG_T, 1> dst_neon(image_width, image_height);
+    Image<IMG_T, 1> dst_cuda(image_width, image_height);
+
+    constexpr auto thresh = 150;
 
     {
         const auto dst = dst_cpp.data();
-        MEASURE(iteration, cpp::threshold, src, dst, width, height, thresh);
+        MEASURE(num_itr, cpp::threshold, src, dst, image_width, image_height, thresh);
     }
 
     {
         const auto dst = dst_neon.data();
-        MEASURE(iteration, neon::threshold, src, dst, width, height, thresh);
+        MEASURE(num_itr, neon::threshold, src, dst, image_width, image_height, thresh);
         compare_images(dst_cpp, dst_neon);
     }
 
     {
-        device_buffer<IMG_T> d_src(width * height, src);
-        device_buffer<IMG_T> d_dst(width * height);
+        device_buffer<IMG_T> d_src(image_width * image_height, src);
+        device_buffer<IMG_T> d_dst(image_width * image_height);
 
-        MEASURE(iteration, cuda::threshold, d_src.get(), d_dst.get(), width, height, thresh);
+        MEASURE(num_itr, cuda::threshold, d_src.get(), d_dst.get(), image_width, image_height, thresh);
 
         d_dst.download(dst_cuda.data());
         compare_images(dst_cpp, dst_cuda);
--- 3/src/threshold_kernel.cu
+++ 3/src/threshold_kernel.cu
@@ -1,6 +1,7 @@
 #include <cuda_runtime.h>
 #include <numeric>
 
+#include "common.hpp"
 #include "cuda_safe_call.hpp"
 #include "threshold_cuda.hpp"
 
@@ -12,13 +13,9 @@ constexpr auto max_value = std::numeric_limits<std::uint8_t>::max();
 
 __global__ void threshold_kernel(const std::uint8_t* const src, std::uint8_t* const dst,
                                  const std::size_t width, const std::size_t height,
-                                 const std::uint8_t thresh, const int pixel_per_thread) {
+                                 const std::uint8_t thresh) {
     const auto idx = blockDim.x * blockIdx.x + threadIdx.x;
-
-    const auto end = min((idx + 1) * pixel_per_thread , static_cast<int>(width * height));
-    for (int i = idx * pixel_per_thread; i < end; i++) {
-        dst[i] = src[i] < thresh ? min_value : max_value;
-    }
+    dst[idx] = src[idx] < thresh ? min_value : max_value;
 }
 
 }  // anonymous namespace
@@ -28,13 +25,10 @@ namespace cuda {
 
 void threshold(const std::uint8_t* const src, std::uint8_t* const dst,
                const std::size_t width, const std::size_t height, const std::uint8_t thresh) {
-    constexpr auto grid_dim = dim3{16};
-    constexpr auto block_dim = dim3{256};
-    const auto pixel_per_thread = static_cast<int>(
-        ceilf(static_cast<float>(width * height) / (grid_dim.x * block_dim.x))
-    );
+    constexpr auto grid_dim = dim3{image_height};
+    constexpr auto block_dim = dim3{image_width};
 
-    threshold_kernel<<<grid_dim, block_dim>>>(src, dst, width, height, thresh, pixel_per_thread);
+    threshold_kernel<<<grid_dim, block_dim>>>(src, dst, width, height, thresh);
     CUDASafeCall();
 }
 
