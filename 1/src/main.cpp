221bed3037c3e27918ee24ef79395f729c6e4d12
yuyuyu <yuyuyu.nttmmoo@gmail.com>
2022-10-08 19:58:55 +0900

    update 1

--- 1/src/main.cpp
+++ 1/src/main.cpp
@@ -11,41 +11,36 @@
 
 
 int main(int argc, char** argv) {
-    if (argc != 5) {
-        std::cout << "usage: " << argv[0] << " image_path width height dump_flag" << std::endl;
+    if (argc != 3) {
+        std::cout << "usage: " << argv[0] << " num_itr dump_flag" << std::endl;
         return 0;
     }
+    const auto num_itr = std::stoi(argv[1]);
+    const auto dump_flag = std::stoi(argv[2]) != 0;
 
-    constexpr auto iteration = 10;
-
-    using IMG_T = std::uint8_t;
-
-    const auto width = std::stoul(argv[2]);
-    const auto height = std::stoul(argv[3]);
-    const auto dump_flag = std::stoi(argv[4]) != 0;
-    const Image<IMG_T, 3> src_img(argv[1], width, height);
+    const Image<IMG_T, 3> src_img(image_color_path, image_width, image_height);
     const auto src = src_img.data();
 
-    Image<IMG_T, 3> dst_cpp(width, height);
-    Image<IMG_T, 3> dst_neon(width, height);
-    Image<IMG_T, 3> dst_cuda(width, height);
+    Image<IMG_T, 3> dst_cpp(image_width, image_height);
+    Image<IMG_T, 3> dst_neon(image_width, image_height);
+    Image<IMG_T, 3> dst_cuda(image_width, image_height);
 
     {
         const auto dst = dst_cpp.data();
-        MEASURE(iteration, cpp::rgb_to_bgr, src, dst, width, height);
+        MEASURE(num_itr, cpp::rgb_to_bgr, src, dst, image_width, image_height);
     }
 
     {
         const auto dst = dst_neon.data();
-        MEASURE(iteration, neon::rgb_to_bgr, src, dst, width, height);
+        MEASURE(num_itr, neon::rgb_to_bgr, src, dst, image_width, image_height);
         compare_images(dst_cpp, dst_neon);
     }
 
     {
-        device_buffer<IMG_T> d_src(width * height * 3, src);
-        device_buffer<IMG_T> d_dst(width * height * 3);
+        device_buffer<IMG_T> d_src(image_width * image_height * 3, src);
+        device_buffer<IMG_T> d_dst(image_width * image_height * 3);
 
-        MEASURE(iteration, cuda::rgb_to_bgr, d_src.get(), d_dst.get(), width, height);
+        MEASURE(num_itr, cuda::rgb_to_bgr, d_src.get(), d_dst.get(), image_width, image_height);
 
         d_dst.download(dst_cuda.data());
         compare_images(dst_cpp, dst_cuda);
--- 1/src/rgb_to_bgr_kernel.cu
+++ 1/src/rgb_to_bgr_kernel.cu
@@ -1,5 +1,6 @@
 #include <cuda_runtime.h>
 
+#include "common.hpp"
 #include "cuda_safe_call.hpp"
 #include "rgb_to_bgr_cuda.hpp"
 
@@ -7,16 +8,12 @@
 namespace {
 
 __global__ void rgb_to_bgr_kernel(const std::uint8_t* const src, std::uint8_t* const dst,
-                                  const std::size_t width, const std::size_t height,
-                                  const int pixel_per_thread) {
+                                  const std::size_t width, const std::size_t height) {
     const auto idx = blockDim.x * blockIdx.x + threadIdx.x;
 
-    const auto end = min((idx + 1) * pixel_per_thread, static_cast<int>(width * height));
-    for (int i = idx * pixel_per_thread; i < end; i++) {
-        dst[i * 3 + 0] = src[i * 3 + 2];
-        dst[i * 3 + 1] = src[i * 3 + 1];
-        dst[i * 3 + 2] = src[i * 3 + 0];
-    }
+    dst[idx * 3 + 0] = src[idx * 3 + 2];
+    dst[idx * 3 + 1] = src[idx * 3 + 1];
+    dst[idx * 3 + 2] = src[idx * 3 + 0];
 }
 
 }  // anonymous namespace
@@ -26,13 +23,13 @@ namespace cuda {
 
 void rgb_to_bgr(const std::uint8_t* const src, std::uint8_t* const dst,
                 const std::size_t width, const std::size_t height) {
-    constexpr auto grid_dim = dim3{16};
-    constexpr auto block_dim = dim3{256};
+    constexpr auto grid_dim = dim3{image_height};
+    constexpr auto block_dim = dim3{image_width};
     const auto pixel_per_thread = static_cast<int>(
         ceilf(static_cast<float>(width * height) / (grid_dim.x * block_dim.x))
     );
 
-    rgb_to_bgr_kernel<<<grid_dim, block_dim>>>(src, dst, width, height, pixel_per_thread);
+    rgb_to_bgr_kernel<<<grid_dim, block_dim>>>(src, dst, width, height);
     CUDASafeCall();
 }
 
