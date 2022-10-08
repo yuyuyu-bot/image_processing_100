1b29fef4a008d3d364af9f60a0c4998d9432b93f
yuyuyu <yuyuyu.nttmmoo@gmail.com>
2022-10-08 21:54:44 +0900

    update 6

--- 6/src/color_reduction_kernel.cu
+++ 6/src/color_reduction_kernel.cu
@@ -2,6 +2,7 @@
 #include <cuda_runtime.h>
 #include <numeric>
 
+#include "common.hpp"
 #include "cuda_safe_call.hpp"
 #include "color_reduction_cuda.hpp"
 
@@ -9,18 +10,14 @@
 namespace {
 
 __global__ void color_reduction_kernel(const std::uint8_t* const src, std::uint8_t* const dst,
-                                       const std::size_t width, const std::size_t height,
-                                       const int pixel_per_thread) {
+                                       const std::size_t width, const std::size_t height) {
     constexpr auto shr = 6u;
     constexpr auto offset = 1 << (shr - 1);
     const auto idx = blockDim.x * blockIdx.x + threadIdx.x;
 
-    const auto end = min((idx + 1) * pixel_per_thread , static_cast<int>(width * height));
-    for (int i = idx * pixel_per_thread; i < end; i++) {
-        dst[i * 3 + 0] = ((src[i * 3 + 0] >> shr) << shr) + offset;
-        dst[i * 3 + 1] = ((src[i * 3 + 1] >> shr) << shr) + offset;
-        dst[i * 3 + 2] = ((src[i * 3 + 2] >> shr) << shr) + offset;
-    }
+    dst[idx * 3 + 0] = ((src[idx * 3 + 0] >> shr) << shr) + offset;
+    dst[idx * 3 + 1] = ((src[idx * 3 + 1] >> shr) << shr) + offset;
+    dst[idx * 3 + 2] = ((src[idx * 3 + 2] >> shr) << shr) + offset;
 }
 
 }  // anonymous namespace
@@ -30,13 +27,10 @@ namespace cuda {
 
 void color_reduction(const std::uint8_t* const src, std::uint8_t* const dst,
                      const std::size_t width, const std::size_t height) {
-    constexpr auto block_dim = dim3{32};
-    constexpr auto thread_dim = dim3{32};
-    constexpr auto num_threads = block_dim.x * thread_dim.x;
-    const auto pixel_per_block =
-        static_cast<int>(ceilf(static_cast<float>(width * height) / num_threads));
+    constexpr auto block_dim = dim3{image_height};
+    constexpr auto thread_dim = dim3{image_width};
 
-    color_reduction_kernel<<<block_dim, thread_dim>>>(src, dst, width, height, pixel_per_block);
+    color_reduction_kernel<<<block_dim, thread_dim>>>(src, dst, width, height);
     CUDASafeCall();
 }
 
