41f9c1f7b64995750fce5e7908f7caf6f833285c
yuyuyu <yuyuyu.nttmmoo@gmail.com>
2022-10-08 21:50:29 +0900

    update 5

--- 5/src/rgb_to_hsv_kernel.cu
+++ 5/src/rgb_to_hsv_kernel.cu
@@ -2,6 +2,7 @@
 #include <cuda_runtime.h>
 #include <numeric>
 
+#include "common.hpp"
 #include "cuda_safe_call.hpp"
 #include "rgb_to_hsv_cuda.hpp"
 
@@ -9,37 +10,33 @@
 namespace {
 
 __global__ void rgb_to_hsv_kernel(const std::uint8_t* const src, std::uint8_t* const dst,
-                                  const std::size_t width, const std::size_t height,
-                                  const int pixel_per_thread) {
+                                  const std::size_t width, const std::size_t height) {
     const auto idx = blockDim.x * blockIdx.x + threadIdx.x;
 
-    const auto end = min((idx + 1) * pixel_per_thread , static_cast<int>(width * height));
-    for (int i = idx * pixel_per_thread; i < end; i++) {
-        const auto R = src[i * 3 + 0];
-        const auto G = src[i * 3 + 1];
-        const auto B = src[i * 3 + 2];
+    const auto R = src[idx * 3 + 0];
+    const auto G = src[idx * 3 + 1];
+    const auto B = src[idx * 3 + 2];
 
-        const auto vmax = max(R, max(G, B));
-        const auto vmin = min(R, min(G, B));
+    const auto vmax = max(R, max(G, B));
+    const auto vmin = min(R, min(G, B));
 
-        int H;
-        if (vmin == vmax) {
-            H = 0;
-        } else if (vmin == B) {
-            H = 60 * (G - R) / (vmax - vmin) + 60;
-        } else if (vmin == R) {
-            H = 60 * (B - G) / (vmax - vmin) + 180;
-        } else {
-            H = 60 * (R - B) / (vmax - vmin) + 300;
-        }
-
-        const auto S = vmax - vmin;
-        const auto V = vmax;
-
-        dst[i * 3 + 0] = static_cast<std::uint8_t>(H / 360.f * 255.f);
-        dst[i * 3 + 1] = S;
-        dst[i * 3 + 2] = V;
+    int H;
+    if (vmin == vmax) {
+        H = 0;
+    } else if (vmin == B) {
+        H = 60 * (G - R) / (vmax - vmin) + 60;
+    } else if (vmin == R) {
+        H = 60 * (B - G) / (vmax - vmin) + 180;
+    } else {
+        H = 60 * (R - B) / (vmax - vmin) + 300;
     }
+
+    const auto S = vmax - vmin;
+    const auto V = vmax;
+
+    dst[idx * 3 + 0] = static_cast<std::uint8_t>(H / 360.f * 255.f);
+    dst[idx * 3 + 1] = S;
+    dst[idx * 3 + 2] = V;
 }
 
 }  // anonymous namespace
@@ -49,13 +46,10 @@ namespace cuda {
 
 void rgb_to_hsv(const std::uint8_t* const src, std::uint8_t* const dst,
                 const std::size_t width, const std::size_t height) {
-    constexpr auto block_dim = dim3{32};
-    constexpr auto thread_dim = dim3{32};
-    constexpr auto num_threads = block_dim.x * thread_dim.x;
-    const auto pixel_per_block =
-        static_cast<int>(ceilf(static_cast<float>(width * height) / num_threads));
+    constexpr auto block_dim = dim3{image_height};
+    constexpr auto thread_dim = dim3{image_width};
 
-    rgb_to_hsv_kernel<<<block_dim, thread_dim>>>(src, dst, width, height, pixel_per_block);
+    rgb_to_hsv_kernel<<<block_dim, thread_dim>>>(src, dst, width, height);
     CUDASafeCall();
 }
 
